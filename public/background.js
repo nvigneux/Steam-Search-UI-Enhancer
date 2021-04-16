8e23430213870f9909699aca895ecfd75238ac23
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2021-04-16 17:14:52 +0200

    [+] steam better ui script

--- public/background.js
+++ public/background.js
@@ -1,9 +1,16 @@
-// dispatch snow status
-const sendSnowStatus = (snowing) => {
-  const message = { type: "SNOW_STATUS", snowing }
+/**
+ * webRequest
+ */
+
+/**
+ *
+ * @param {string} type
+ * @param {object} payload
+ */
+const sendTabsMessage = (message) => {
   chrome.runtime.sendMessage(message)
 
-  // send message to every active tab
+  // send requestStatus to every active tab
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab) => {
       if (tab.id) chrome.tabs.sendMessage(tab.id, message)
@@ -11,21 +18,102 @@ const sendSnowStatus = (snowing) => {
   })
 }
 
+const tabStorage = {}
+const networkFilters = {
+  urls: [
+    "*://store.steampowered.com/search",
+    "*://store.steampowered.com/search/*",
+  ],
+}
+
+// onActivated
+chrome.tabs.onActivated.addListener((tab) => {
+  const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE
+  if (!tabStorage[tabId]) {
+    tabStorage[tabId] = {
+      id: tabId,
+      requests: {},
+      registerTime: new Date().getTime(),
+    }
+  }
+})
+
+// onBeforeRequest
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+  const { tabId, requestId } = details
+  if (!tabStorage[tabId]) return
+
+  tabStorage[tabId].requests[requestId] = {
+    requestId,
+    url: details.url,
+    startTime: details.timeStamp,
+    status: "pending",
+  }
+}, networkFilters)
+
+// onCompleted
+chrome.webRequest.onCompleted.addListener((details) => {
+  const { tabId, requestId } = details
+  if (!tabStorage[tabId] || !tabStorage[tabId].requests[requestId]) return
+
+  const request = tabStorage[tabId].requests[requestId]
+
+  Object.assign(request, {
+    endTime: details.timeStamp,
+    requestDuration: details.timeStamp - request.startTime,
+    status: "complete",
+  })
+
+  sendTabsMessage({ type: "REQUEST_SEARCH_COMPLETED", request })
+
+  console.log(tabStorage[tabId].requests[details.requestId])
+}, networkFilters)
+
+// onRemoved
+chrome.tabs.onRemoved.addListener((tab) => {
+  const { tabId } = tab
+  if (!tabStorage[tabId]) return
+
+  tabStorage[tabId] = null
+})
+
+/**
+ * onMessage
+ * Snowing
+ */
+
+// dispatch snow status
+// const sendSnowStatus = (snowing) => {
+//   const message = { type: "SNOW_STATUS", snowing }
+//   chrome.runtime.sendMessage(message)
+
+//   // send message to every active tab
+//   chrome.tabs.query({}, (tabs) => {
+//     tabs.forEach((tab) => {
+//       if (tab.id) chrome.tabs.sendMessage(tab.id, message)
+//     })
+//   })
+// }
+
 let snowing = false
 
 // Get locally stored value
 chrome.storage.local.get("snowing", (res) => !!res.snowing)
 
 // event reducer to send event to content.js
+// listen content and app.jsx
 chrome.runtime.onMessage.addListener((message) => {
   switch (message.type) {
     case "REQ_SNOW_STATUS":
-      sendSnowStatus(snowing)
+      sendTabsMessage({ type: "SNOW_STATUS", snowing })
       break
     case "TOGGLE_SNOW":
       snowing = message.snowing
       chrome.storage.local.set({ snowing })
-      sendSnowStatus(snowing)
+      sendTabsMessage({ type: "SNOW_STATUS", snowing })
+      break
+    case "REQ_COMPLETED_REQUEST":
+      sendTabsMessage({ type: "REQUEST_SEARCH_COMPLETED", request: {} })
       break
     default:
       break
--- public/content.js
+++ public/content.js
@@ -1,5 +1,13 @@
+const regexNumber = /(\d{0,3},)?(\d{3},)?(\d+)/g
+const setAttributes = (el, attrs) => {
+  Object.keys(attrs).forEach((key) => {
+    if (key && attrs[key]) el.setAttribute(key, attrs[key])
+  })
+}
+/**
+ * Snowing
+ */
 const body = document.getElementsByTagName("body")
-
 const snowflakesContainer = document.createElement("div")
 snowflakesContainer.className = "snowflakes"
 snowflakesContainer.setAttribute("aria-hidden", "true")
@@ -7,25 +15,114 @@ snowflakesContainer.setAttribute("aria-hidden", "true")
 const snowflake = document.createElement("div")
 snowflake.className = "snowflake"
 snowflake.innerHTML = "❆"
-
 for (let i = 0; i < 12; i += 1) {
   snowflakesContainer.appendChild(snowflake.cloneNode(true))
 }
 
-chrome.runtime.sendMessage({ type: "REQ_SNOW_STATUS" })
+/**
+ * Steam rating UI
+ */
 
+const applyBetterScoreUi = (row, { percent, reviews }) => {
+  const div = document.createElement("div")
+  const container = row.querySelector(
+    ".search_reviewscore.responsive_secondrow"
+  )
+  const img = row.querySelector(".search_capsule img")
+
+  setAttributes(container, {
+    style: `position:relative;`,
+  })
+  setAttributes(img, {
+    style: "position:relative;transform: translate(0, 15%);",
+  })
+  setAttributes(div, {
+    style:
+      "font-size:10px;position:absolute;width:max-content;left:21%;bottom:-10px;",
+  })
+
+  div.innerHTML += `${percent}% - ${reviews} reviews`
+  container.appendChild(div)
+}
+
+const reviewColor = (percent, reviews) => {
+  if (percent <= 100 && percent >= 95) return "var(--color-positive-1)"
+  if (percent <= 95 && percent >= 85) return "var(--color-positive-2)"
+  if (percent <= 85 && percent >= 80) return "var(--color-positive-3)"
+  if (percent <= 79 && percent >= 70) return "var(--color-positive-4)"
+  if (percent <= 69 && percent >= 40) return "var(--color-mixed-1)"
+  if (percent <= 39 && percent >= 20) return "var(--color-negative-4)"
+  if (percent <= 19 && percent >= 0 && reviews >= 500000)
+    return "var(--color-negative-3"
+  if (percent <= 19 && percent >= 0 && reviews <= 500000 && reviews >= 50000)
+    return "var(--color-negative-2"
+  if (percent <= 19 && percent >= 0 && reviews <= 50000 && reviews >= 1)
+    return "var(--color-negative-1"
+}
+
+const applyBetterStyle = (row, { percent, reviews }) => {
+  setAttributes(row, {
+    style: `border-right: 6px solid ${reviewColor(
+      Number(percent),
+      Number(reviews.replace(/,/g, ""))
+    )}`,
+  })
+}
+
+const applyRatingUi = () => {
+  const searchRows = [
+    ...document.querySelectorAll(".search_result_row:not(.steam-better-ui)"),
+  ]
+  searchRows.map((row) => {
+    row.classList.add("steam-better-ui")
+    const review = row.querySelector(".search_review_summary")
+    if (review && review.hasAttribute("data-tooltip-html")) {
+      const reviewStats = review.dataset.tooltipHtml.match(regexNumber)
+
+      if (reviewStats && reviewStats.length) {
+        if (true)
+          applyBetterScoreUi(row, {
+            percent: reviewStats[0],
+            reviews: reviewStats[1],
+          })
+
+        if (true)
+          applyBetterStyle(row, {
+            percent: reviewStats[0],
+            reviews: reviewStats[1],
+          })
+      }
+    }
+  })
+}
+
+// const applyBetterStyle = (rows) => {}
+
+// Const init
 let snowing = false
+
+// Runtime
+chrome.runtime.sendMessage({ type: "REQ_SNOW_STATUS" })
+chrome.runtime.sendMessage({ type: "REQ_COMPLETED_REQUEST" })
+
+// onMessage
 chrome.runtime.onMessage.addListener((message) => {
+  console.log(message)
   switch (message.type) {
     case "SNOW_STATUS":
       if (message.snowing) {
         if (!snowing) {
           body[0]?.prepend(snowflakesContainer)
         }
-      } else {
+      }
+
+      if (!message.snowing) {
         snowflakesContainer.parentNode?.removeChild(snowflakesContainer)
       }
       snowing = message.snowing
+      break
+    case "REQUEST_SEARCH_COMPLETED":
+      applyRatingUi()
       break
     default:
       break
